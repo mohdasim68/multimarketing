5b59f50cd90afdb672010ea991d5f40045900950
mohdasim68 <asimmohd68@gmail.com>
2020-02-06 22:16:06 +0530

    first commit

---
+++ billionaire/js/temp.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar').toggleClass('active');
+    });
+});
+
+
+// chart colors
+var colors = ['#27AAF0', '#28a745', '#333333', '#c3e6cb', '#dc3545', '#6c757d'];
+
+
+
+/* bar chart */
+var chBar = document.getElementById("chBar");
+if (chBar) {
+    new Chart(chBar, {
+        type: 'bar',
+        data: {
+            labels: ["S", "M", "T", "W", "T", "F", "S"],
+            datasets: [{
+                data: [10, 8, 30, 40, 20, 50, 70],
+                backgroundColor: colors[0]
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    barPercentage: 0.4,
+                    categoryPercentage: 0.5
+                }]
+            }
+        }
+    });
+}
